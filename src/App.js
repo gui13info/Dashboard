b5731bef29cf5c73a88f5b3849c479af27f6b967
Guilherme Marques <marques.guilherme0067@gmail.com>
2020-09-23 20:42:51 -0300

    Definindo interface do Resumo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
+import Resumo from './components/Resumo/Resumo';
 
 function App() {
   return (
     <div>
       <MenuSuperior />
+      <Resumo />
     </div>
   );
 }
