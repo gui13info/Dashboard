b32e01f8ace88eb5c0f71bdee3b6d9bf623e945d
Guilherme Marques <marques.guilherme0067@gmail.com>
2020-09-28 21:43:52 -0300

    Criando interfance de Consultas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
 import Resumo from './components/Resumo/Resumo';
+import Consultas from './components/Consultas/Consultas';
 
 function App() {
   return (
     <div>
       <MenuSuperior />
       <Resumo />
+      <Consultas />
     </div>
   );
 }
--- src/components/Consultas/Consultas.js
+++ src/components/Consultas/Consultas.js
@@ -17,7 +17,65 @@ class Consultas extends Component{
     render(){
         return(
             <div>
-                
+                <h2 className="mt-2">Consultas</h2>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="card mt-2">
+                            <div className="card-header">
+                                Realizadas
+                            </div>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Especialidade</th>
+                                        <th>Quantidade</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.realizadas.map((item, indice) => {
+                                            return (
+                                                <tr key={indice}>
+                                                    <td>{item.especialidade}</td>
+                                                    <td>{item.quantidade}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    
+                    <div className="col">
+                        <div className="card mt-2">
+                            <div className="card-header">
+                                Marcadas
+                            </div>
+                            <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Especialidade</th>
+                                    <th>Quantidade</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.marcadas.map((item, indice) => {
+                                        return (
+                                            <tr key={indice}>
+                                                <td>{item.especialidade}</td>
+                                                <td>{item.quantidade}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
